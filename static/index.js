45fd8de8b4b0b2946f44de92ca0d7900f0788325
Matt Hughes <mhughes@edx.org>
2018-11-21 10:24:32 -0500

    Leverage desktop-emulating endpoints

--- static/index.js
+++ static/index.js
@@ -1,28 +1,47 @@
 import { handlerWrapper } from '@edx/edx-proctoring';
 
+const makeRequest = ({url, method}) => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = function() {
+      if (this.status >= 200 && this.status < 300) {
+        resolve(JSON.parse(xhr.response));
+      } else {
+        const { status } = this;
+        const { statusText } = xhr;
+        reject({ status, statusText });
+      }
+    };
+    xhr.onerror = function() {
+      const { status } = this;
+      const { statusText } = xhr;
+      reject({ status, statusText });
+    };
+    xhr.send();
+  });
+};
+
 console.log('hello from MockProck!');
 
 class MockProctoringEventHandler {
-  constructur({baseUrl = 'localhost'}) {
+  constructor({baseUrl = 'http://localhost:11136'}) {
     this.baseUrl = baseUrl;
   }
   onStartExamAttempt() {
-    return new Promise(function(resolve, reject) {
-      console.log("MockProctoringEventHandler - onStartExamAttempt() called");
-      setTimeout(resolve, 1000);
-    });
+    console.log("MockProctoringEventHandler - onStartExamAttempt() called");
+    return makeRequest({url: `${this.baseUrl}/desktop/start`, method: 'POST'})
+      .then(response => response.status === "running" ? Promise.resolve() : Promise.reject());
   }
   onEndExamAttempt() {
-    return new Promise((resolve, reject) => {
-      console.log("MockProctoringEventHandler - onEndExamAttempt() called");
-      setTimeout(resolve, 1000);
-    });
+    console.log("MockProctoringEventHandler - onEndExamAttempt() called");
+    return makeRequest({url: `${this.baseUrl}/desktop/stop`, method: 'POST'})
+      .then(response => response.status === "uploading" ? Promise.resolve() : Promise.reject());
   }
   onPing() {
-    return new Promise((resolve) => {
-      console.log("MockProctoringEventHandler - onPing() called");
-      setTimeout(resolve, 100);
-    });
+    console.log("MockProctoringEventHandler - onPing() called");
+    return makeRequest({url: `${this.baseUrl}/desktop/ping`, method: 'GET'})
+      .then(response => response.status === "running" ? Promise.resolve() : Promise.reject());
   }
 }
 
